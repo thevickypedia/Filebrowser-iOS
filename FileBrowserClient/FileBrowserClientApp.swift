864e95506b97655f93f292c3a4c37e9cd610fd20
Vignesh Rao <svignesh1793@gmail.com>
2025-07-06 14:00:36 -0500

    feat: Setup a base hello world project

---
+++ FileBrowserClient/FileBrowserClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileBrowserClientApp.swift
+//  FileBrowserClient
+//
+//  Created by Vignesh Rao on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FileBrowserClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
