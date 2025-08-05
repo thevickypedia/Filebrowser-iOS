89c4c26f472c57efe5ddbd825d6fdba1f09bab8f
Vignesh Rao <svignesh1793@gmail.com>
2025-08-05 16:00:18 -0500

    perf: Conditionally load ``AVPlayer`` only when visible

--- FileBrowserClient/FileBrowserClient/MediaPlayerView.swift
+++ FileBrowserClient/FileBrowserClient/MediaPlayerView.swift
@@ -14,6 +14,7 @@ struct MediaPlayerView: View {
     let serverURL: String
     let token: String
     @State private var player: AVPlayer? = nil
+    @State private var isVisible = false
 
     var body: some View {
         Group {
@@ -24,26 +25,30 @@ struct MediaPlayerView: View {
             }
         }
         .onAppear {
-            if player == nil {
-                let path = file.path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
-                if let url = URL(string: "\(serverURL)/api/raw/\(removePrefix(urlPath: path))?auth=\(token)") {
-                    DispatchQueue.global(qos: .userInitiated).async {
-                        let asset = AVURLAsset(url: url)
-                        let item = AVPlayerItem(asset: asset)
-                        let loadedPlayer = AVPlayer(playerItem: item)
-                        DispatchQueue.main.async {
-                            self.player = loadedPlayer
-                            loadedPlayer.pause()
-                        }
-                    }
-                }
-            }
+            isVisible = true
+            if player == nil && isVisible { loadPlayer() }
         }
         .onDisappear {
+            isVisible = false
             player?.pause()
             player?.replaceCurrentItem(with: nil)
             player = nil
         }
         .navigationTitle(file.name)
+    }
+
+    func loadPlayer() {
+        let path = file.path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+        if let url = URL(string: "\(serverURL)/api/raw/\(removePrefix(urlPath: path))?auth=\(token)") {
+            DispatchQueue.global(qos: .userInitiated).async {
+                let asset = AVURLAsset(url: url)
+                let item = AVPlayerItem(asset: asset)
+                let loadedPlayer = AVPlayer(playerItem: item)
+                DispatchQueue.main.async {
+                    self.player = loadedPlayer
+                    loadedPlayer.pause()
+                }
+            }
+        }
     }
 }
