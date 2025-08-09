f65ba6a0f70429f1e54ccf616f23e17f23ec0568
Vignesh Rao <svignesh1793@gmail.com>
2025-08-09 11:03:09 -0500

    fix: Remove automatic session restoration

--- FileBrowserClient/FileBrowserClient/FileBrowserClientApp.swift
+++ FileBrowserClient/FileBrowserClient/FileBrowserClientApp.swift
@@ -18,32 +18,6 @@ struct FileBrowserClientApp: App {
                 .environmentObject(authManager)
                 .environmentObject(themeManager)
                 .preferredColorScheme(themeManager.colorScheme)
-                .onAppear {
-                    attemptSessionRestore()
-                }
-        }
-    }
-
-    func attemptSessionRestore() {
-        // Only attempt biometric restore if user explicitly enabled it
-        guard UserDefaults.standard.bool(forKey: "useFaceID") else { return }
-
-        KeychainHelper.authenticateWithBiometrics { success in
-            guard success,
-                  let session = KeychainHelper.loadSession(),
-                  let token = session["token"],
-                  let username = session["username"],
-                  let serverURL = session["serverURL"] else {
-                return
-            }
-            DispatchQueue.main.async {
-                authManager.token = token
-                authManager.username = username
-                authManager.serverURL = serverURL
-                Task {
-                    await authManager.fetchPermissions(for: username, token: token, serverURL: serverURL)
-                }
-            }
         }
     }
 }
