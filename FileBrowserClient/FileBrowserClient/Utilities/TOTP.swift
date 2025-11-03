748bfb3d2187579c5aa46da1a03ed3b0a0a9f264
Vignesh Rao <svignesh1793@gmail.com>
2025-11-02 22:51:42 -0600

    lint: Run linter to fix line breaks

--- FileBrowserClient/FileBrowserClient/Utilities/TOTP.swift
+++ FileBrowserClient/FileBrowserClient/Utilities/TOTP.swift
@@ -14,13 +14,13 @@ func base32Decode(_ base32: String) -> Data? {
     var bits = ""
     var output = Data()
     let cleaned = base32.uppercased().replacingOccurrences(of: "=", with: "")
-    
+
     for char in cleaned {
         guard let index = alphabet.firstIndex(of: char) else { return nil }
         let value = alphabet.distance(from: alphabet.startIndex, to: index)
         bits += String(value, radix: 2).leftPadding(toLength: 5, withPad: "0")
     }
-    
+
     // Walk the bit string in 8-bit chunks
     var i = bits.startIndex
     while i < bits.endIndex {
@@ -31,7 +31,7 @@ func base32Decode(_ base32: String) -> Data? {
         }
         i = end
     }
-    
+
     return output
 }
 
@@ -50,21 +50,21 @@ func generateTOTP(secret: String,
                   digits: Int = 6,
                   period: TimeInterval = 30,
                   algorithm: HMAC<Insecure.SHA1>.Type = HMAC<Insecure.SHA1>.self) -> String? {
-    
+
     guard let keyData = base32Decode(secret) else { return nil }
     let counter = UInt64(floor(time.timeIntervalSince1970 / period))
-    
+
     var bigEndianCounter = counter.bigEndian
     let counterData = Data(bytes: &bigEndianCounter, count: MemoryLayout<UInt64>.size)
-    
+
     let hmac = HMAC<Insecure.SHA1>.authenticationCode(for: counterData, using: SymmetricKey(data: keyData))
     let hash = Data(hmac)
-    
+
     let offset = Int(hash.last! & 0x0f)
     let truncatedHash = hash.subdata(in: offset..<(offset + 4))
     var number = truncatedHash.withUnsafeBytes { $0.load(as: UInt32.self) }
     number = UInt32(bigEndian: number) & 0x7fffffff
-    
+
     let otp = number % UInt32(pow(10, Float(digits)))
     return String(format: "%0*u", digits, otp)
 }
