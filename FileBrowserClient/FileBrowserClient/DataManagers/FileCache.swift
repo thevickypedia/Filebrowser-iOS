66541a3d90b8bae798483da71ca335625d3f392c
Vignesh Rao <svignesh1793@gmail.com>
2026-02-03 17:57:03 -0600

    feat: Remove serverURL from file cache path to store/retrieve/clear cache for all servers at once

--- FileBrowserClient/FileBrowserClient/DataManagers/FileCache.swift
+++ FileBrowserClient/FileBrowserClient/DataManagers/FileCache.swift
@@ -19,21 +19,15 @@ class FileCache {
         try? fileManager.createDirectory(at: diskCacheURL, withIntermediateDirectories: true)
     }
 
-    private func cacheKeyPrefix(for server: String) -> String {
-        let safeServer = sanitizer(server.trimmingCharacters(in: CharacterSet(charactersIn: ".")))
-        return "cache-\(safeServer)-"
-    }
-
-    private func cacheKey(for server: String, path: String, modified: String?, fileID: String?) -> String {
+    private func cacheKey(path: String, modified: String?, fileID: String?) -> String {
         let safePath = sanitizer(path)
-        let safeServer = sanitizer(server.trimmingCharacters(in: CharacterSet(charactersIn: ".")))
         let safeFileID = sanitizer(fileID?.trimmingCharacters(in: CharacterSet(charactersIn: ".")) ?? "unknown")
         let safeModified = sanitizer(modified ?? "unknown", replacement: "-")
-        return "cache-\(safeServer)-\(safePath)-\(safeFileID)-\(safeModified)"
+        return "cache-\(safePath)-\(safeFileID)-\(safeModified)"
     }
 
     func retrieve(for server: String, path: String, modified: String?, fileID: String?) -> Data? {
-        let key = cacheKey(for: server, path: path, modified: modified, fileID: fileID)
+        let key = cacheKey(path: path, modified: modified, fileID: fileID)
         let diskPath = diskCacheURL.appendingPathComponent(key)
         var result: Data?
         DispatchQueue.global(qos: .utility).sync {
@@ -46,7 +40,7 @@ class FileCache {
     }
 
     func store(for server: String, data: Data, path: String, modified: String?, fileID: String?) {
-        let key = cacheKey(for: server, path: path, modified: modified, fileID: fileID)
+        let key = cacheKey(path: path, modified: modified, fileID: fileID)
         let diskPath = diskCacheURL.appendingPathComponent(key)
         DispatchQueue.global(qos: .utility).async {
             if (try? data.write(to: diskPath)) != nil {
@@ -72,21 +66,8 @@ class FileCache {
             Log.info("No stored cache available!")
             return
         }
-        // let cachePrefix = serverURL.map { cacheKeyPrefix(for: $0) } ?? diskCacheURL
-        var cachePrefix: String
-        if let server = serverURL {
-            cachePrefix = cacheKeyPrefix(for: server)
-            Log.info("Clearing local cache for: \(server)")
-        } else {
-            Log.info("Deleting ALL server cache")
-            cachePrefix = ""
-        }
         for url in urls {
-            // Use ".path" for faster comparison (vs ".absoluteString")
-            let cacheURL = url.path.components(separatedBy: "/").last ?? ""
-            if cacheURL.hasPrefix(cachePrefix) {
-                try? fileManager.removeItem(at: url)
-            }
+            try? fileManager.removeItem(at: url)
         }
     }
 
